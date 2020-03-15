8fb96c4115f3b160879f2f5d7df1b6b98cac8850
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-15 10:16:08 -0500

    Create reports_json_maker.py

---
+++ Python/Secrets/reports_json_maker.py
@@ -0,0 +1,26 @@
+#!/bin/python
+
+import json
+
+# example dictionary that contains data like you want to have in json
+details = 
+{
+  "report": {
+    "all_cases": "ALL_CASES_REPORT_ID",
+    "all_cases_change_requests": "ALL_CASES_CHANGE_REQUESTS_ID",
+    "all_cases_handover": "ALL_CASES_HANDOVER_ID",
+    "all_cases_sev1": "ALL_CASES_SEV1_ID",
+    "all_cases_sev2": "ALL_CASES_SEV2_ID",
+    "all_cases_sev3": "ALL_CASES_SEV3_ID",
+    "all_cases_sev4": "ALL_CASES_SEV4_ID"
+  }
+}
+
+with open('reports.json', 'w') as json_file:
+    json.dump(details, json_file, indent=4)
+
+# get json string from that dictionary
+json = json.dumps(details, indent=4)
+print("JSON will be STD.OUT to secrets.json")
+print("Please check the current working directory for the file.")
+print json
