cadabb5a8defd46acdd38fe95a2af8e796357f6b
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-15 10:10:39 -0500

    Create secrets_json_maker.py

---
+++ Python/Secrets/secrets_json_maker.py
@@ -0,0 +1,21 @@
+#!/bin/python
+
+import json
+
+# example dictionary that contains data like you want to have in json
+details = {
+  "user": {
+    "username": "INSERT_USERNAME",
+    "password": "INSERT_PASSWORD",
+    "salesforce_token": "INSERT_TOKEN"
+  }
+}
+
+with open('secrets.json', 'w') as json_file:
+    json.dump(details, json_file, indent=4)
+
+# get json string from that dictionary
+json = json.dumps(details, indent=4)
+print("JSON will be STD.OUT to secrets.json")
+print("Please check the current working directory for the file.")
+print json
