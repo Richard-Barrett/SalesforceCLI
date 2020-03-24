bf7352b85e20d224c8efdebd122cf08109eb4147
Richard Barrett <rbarrett@mirantis.com>
2020-03-24 15:42:59 -0500

    Chmodded Scripts as Executable and Fixed some links

--- Integration/Slack/automated_messages.sh
+++ Integration/Slack/automated_messages.sh
@@ -10,16 +10,16 @@
 # curl -X POST -H 'Content-type: application/json' --data '{"text":"BODY"}' https://hooks.slack.com/services/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
 
 # Generalt Message:
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Please see the follwing links for Handovers and Change Requests that may impact your shift."}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Please see the following links for Handovers and Change Requests that may impact your shift."}' https://hooks.slack.com/services/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
 
 # Messages for Handover:
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Handovers: https://mirantis.my.salesforce.com/00O2S000003g25h"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Handovers: https://mirantis.my.salesforce.com/00O2S000003g25h"}' https://hooks.slack.com/services/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
 
 # Message for All Change Requests:
 # https://mirantis.my.salesforce.com/00O2S000004INH1?cancelURL=%2F00O2S000004INH1
-curl -X POST -H 'Content-type: application/json' --data '{"text":"All Change Requests: https://mirantis.my.salesforce.com/00O2S000004INH1?cancelURL=%2F00O2S000004INH1"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"All Change Requests: https://mirantis.my.salesforce.com/00O2S000004INH1"}' https://hooks.slack.com/services/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
 
 # Message for Change Requests in Ready to Execute
 # https://mirantis.my.salesforce.com/00O2S000004ISIs
 # curl -X POST -H 'Content-type: application/json' --data '{"text":"All CRs in Ready to Execute:
-https://mirantis.my.salesforce.com/00O2S000004ISIs"}' https://hooks.slack.com/services/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
+https://mirantis.my.salesforce.com/00O2S000004ISIs"}' https://hooks.slack.com/services/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
