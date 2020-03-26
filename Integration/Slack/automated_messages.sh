1e3e9c1ce3b813136414a9850dd696b9127b66f6
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-03-26 16:16:13 -0500

    Update automated_messages.sh

--- Integration/Slack/automated_messages.sh
+++ Integration/Slack/automated_messages.sh
@@ -8,20 +8,16 @@
 # ===========================================================
 
 # System Variables
-webhook_url="$(jq --raw-output .slack_config.slack_target_url secrets.json)"
+webhook_url=$(cat secrets.json | jq ".slack_config.slack_target_url" | tr -d \")
 
 # Use Messages in this command syntax
-# curl -X POST -H 'Content-type: application/json' --data '{"text":"BODY"}' <insert_URL>
-# curl -X POST -H 'Content-type: application/json' --data '{"text":"BODY"}' && echo '{"slack_config": {"slack_target_url": "URL"}}' | jq --raw-output .slack_config.slack_target_url
+# curl -X POST -H 'Content-type: application/json' --data '{"text":"MESSAGE TO INSERT"}' $webhook_url
 
 # Generalt Message:
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Please see the following links for Handovers and Change Requests that may impact your shift."}' && echo '{"slack_config": {"slack_target_url": "URL"}}' | jq --raw-output .slack_config.slack_target_url
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Please see the following links for Handovers and Change Requests that may impact your shift."}' $webhook_url
 
 # Messages for Handover:
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Handovers: https://mirantis.my.salesforce.com/00O2S000003g25h"}' && echo '{"slack_config": {"slack_target_url": "URL"}}' | jq --raw-output .slack_config.slack_target_url
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Handovers: https://mirantis.my.salesforce.com/00O2S000003g25h"}' $webhook_url
 
 # Message for All Change Requests:
-curl -X POST -H 'Content-type: application/json' --data '{"text":"All Change Requests: https://mirantis.my.salesforce.com/00O2S000004INH1"}' && echo '{"slack_config": {"slack_target_url": "URL"}}' | jq --raw-output .slack_config.slack_target_url
-
-# Message for Change Requests in Ready to Execute
-# curl -X POST -H 'Content-type: application/json' --data '{"text":"All CRs in Ready to Execute:"}' <insert_URL>
+curl -X POST -H 'Content-type: application/json' --data '{"text":"All Change Requests: https://mirantis.my.salesforce.com/00O2S000004INH1"}' $webhook_url
