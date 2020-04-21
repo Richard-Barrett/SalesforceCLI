0a87155a57ccd2352191a7663f309fe866168acf
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-04-21 12:49:57 -0500

    Create sfdc.py

---
+++ Python/sfdc.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import argparse
+
+parser = argparse.ArgumentParser(
+	description='SalesforceCLI will return Pandas Dataframes from Salesforce Cases within an Organizations SFDC. It will also allow you to interact with Salesforce Lighting Experience or Service console from within the CLI. You will even be able to make leads, create cases, and send off emails all from your CLI!',
+	epilog="SalesforceCLI is here to help you automate reports and data within your Organizations SFDC"
+)
+
+# Poitional Arguments
+parser.add_argument('accounts', help='Pandas Dataframe that shows all available accounts active within an organizational SFDC')
+parser.add_argument('cases', help='cases dataframes related to defined case report, default is set to all cases')
+parser.add_argument('contacts', help='return a list of contacts as a dataframe')
+parser.add_argument('leads', help='leads dataframes related to all defined leads for user access, default is set to all concurrent leads within an organizational SFDC')
+parser.add_argument('lightning', help='Work with Salesforce Lightning from the CLI')
+parser.add_argument('service', help='Work with Salesforce Service Console from the CLI')
+parser.add_argument('soql', help='SOQL custom query for users within an SFDC')
+parser.add_argument('reports', help='reports dataframes related to defined reporst, default is set to list all available reports for use with SFDC access')
+
+# Optional Arguments
+parser.add_argument('-v','--version', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='Returns the version of SalesforceCLI'),
+#printf("Optional Arguments for cases")
+parser.add_argument('-s1','--sev1', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='Return Pandas Dataframe for all Severity Level 1 Cases')
+parser.add_argument('-s2','--sev2', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='Return Pandas Dataframe for all Severity Level 2 Cases')
+parser.add_argument('-s3','--sev3', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='Return Pandas Dataframe for all Severity Level 3 Cases')
+parser.add_argument('-s4','--sev4', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='Return Pandas Dataframe for all Severity Level 4 Cases')
+
+args = parser.parse_args()
+
+if args.sev1:
+   execfile('~/Git/SalesforceCLI/Python/Case/read_all_sev1_cases.py')
+
+if args.sev2:
+   execfile('~/Git/SalesforceCLI/Python/Cases/read_all_sev2_cases.py')
