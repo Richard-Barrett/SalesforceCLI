4965edd4183d065e1e482bb1bb66da4ebc3dec82
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2020-04-23 09:17:03 -0500

    Update sfdc.py

--- Python/sfdc.py
+++ Python/sfdc.py
@@ -16,14 +16,14 @@ parser = argparse.ArgumentParser(
 )
 
 # Poitional Arguments
-parser.add_argument('accounts', help='Pandas Dataframe that shows all available accounts active within an organizational SFDC')
+#parser.add_argument('accounts', help='Pandas Dataframe that shows all available accounts active within an organizational SFDC')
 parser.add_argument('cases', help='cases dataframes related to defined case report, default is set to all cases')
-parser.add_argument('contacts', help='return a list of contacts as a dataframe')
-parser.add_argument('leads', help='leads dataframes related to all defined leads for user access, default is set to all concurrent leads within an organizational SFDC')
-parser.add_argument('lightning', help='Work with Salesforce Lightning from the CLI')
-parser.add_argument('service', help='Work with Salesforce Service Console from the CLI')
-parser.add_argument('soql', help='SOQL custom query for users within an SFDC')
-parser.add_argument('reports', help='reports dataframes related to defined reporst, default is set to list all available reports for use with SFDC access')
+#parser.add_argument('contacts', help='return a list of contacts as a dataframe')
+#parser.add_argument('leads', help='leads dataframes related to all defined leads for user access, default is set to all concurrent leads within an organizational SFDC')
+#parser.add_argument('lightning', help='Work with Salesforce Lightning from the CLI')
+#parser.add_argument('service', help='Work with Salesforce Service Console from the CLI')
+#parser.add_argument('soql', help='SOQL custom query for users within an SFDC')
+#parser.add_argument('reports', help='reports dataframes related to defined reporst, default is set to list all available reports for use with SFDC access')
 
 # Optional Arguments
 parser.add_argument('-v','--version', action='store_true',
@@ -31,12 +31,12 @@ parser.add_argument('-v','--version', action='store_true',
 #printf("Optional Arguments for cases")
 parser.add_argument('-s1','--sev1', action='store_true',
                     help='Return Pandas Dataframe for all Severity Level 1 Cases'),
-#parser.add_argument('-s2','--sev2', action='store_true',
-#                    help='Return Pandas Dataframe for all Severity Level 2 Cases'),
-#parser.add_argument('-s3','--sev3', action='store_true', 
-#		    help='Return Pandas Dataframe for all Severity Level 3 Cases'),
-#parser.add_argument('-s4','--sev4', action='store_true',
-#		    help='Return Pandas Dataframe for all Severity Level 4 Cases')
+parser.add_argument('-s2','--sev2', action='store_true',
+                    help='Return Pandas Dataframe for all Severity Level 2 Cases'),
+parser.add_argument('-s3','--sev3', action='store_true', 
+		    help='Return Pandas Dataframe for all Severity Level 3 Cases'),
+parser.add_argument('-s4','--sev4', action='store_true',
+		    help='Return Pandas Dataframe for all Severity Level 4 Cases')
 args = parser.parse_args()
 
 if args.sev1:
